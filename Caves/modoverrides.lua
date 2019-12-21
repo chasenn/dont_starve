f935e7b9ea37f2917b7094aee0c75d4b97ca6ac6
chasenn <chasenn@foxmail.com>
2019-12-21 11:48:36 +0800

    11

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,84 @@
+return {
+  ["workshop-1937912794"]={ configuration_options={  }, enabled=true },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-356930882"]={ configuration_options={ uses=10000000, uses2=10000000 }, enabled=true },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-382177939"]={
+    configuration_options={ [""]=true, chillit="yep", eightxten="5x16", workit="yep" },
+    enabled=true 
+  },
+  ["workshop-458940297"]={
+    configuration_options={
+      DFV_ClientPrediction="default",
+      DFV_FueledSettings="default",
+      DFV_Language="EN",
+      DFV_MinimalMode="default",
+      DFV_PercentReplace="default",
+      DFV_ShowACondition="default",
+      DFV_ShowADefence="default",
+      DFV_ShowAType="default",
+      DFV_ShowDamage="default",
+      DFV_ShowFireTime="default",
+      DFV_ShowInsulation="default",
+      DFV_ShowTemperature="default",
+      DFV_ShowUses="default" 
+    },
+    enabled=true 
+  },
+  ["workshop-460972875"]={ configuration_options={ LANG="simplified" }, enabled=true },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-569043634"]={ configuration_options={  }, enabled=true },
+  ["workshop-572538624"]={
+    configuration_options={
+      IS_CHS_ALL_MOD=true,
+      IS_CHS_CHARACTER=true,
+      IS_CHS_FIX_ALL=true,
+      IS_CHS_SETTINGS=true 
+    },
+    enabled=true 
+  },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="DestroyByAll",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-786556008"]={
+    configuration_options={ ENABLEBACKPACK=false, EXTRASLOT=0, INVENTORYSIZE=45 },
+    enabled=true 
+  } 
+}
